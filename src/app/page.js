e6b0ca3860ac962a47285b121ef8eb16edc34214
mahirgautam <165561059+mahirgautam@users.noreply.github.com>
2025-01-11 21:38:52 +0530

    15th commit

--- src/app/page.js
+++ src/app/page.js
@@ -6,24 +6,28 @@ import SectionHeaders from "../components/layout/SectionHeaders";
 export default function Home() {
   return (
     <>
-      
-      <Hero/>
-      <HomeMenu/>
+
+      <Hero />
+      <HomeMenu />
       <section className="text-center my-16" id="about">
         <SectionHeaders subHeader={'Our Story'} mainHeader={'About us'} />
         <div className="max-w-md mx-auto mt-4 text-gray-500 flex flex-col gap-4">
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum aspernatur voluptatem vitae eos veritatis necessitatibus ad similique ratione et mollitia eveniet iusto accusamus, dolorum adipisci dolor cumque provident velit soluta.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore eum assumenda ullam ipsam officia ipsum. Fuga quae vero distinctio explicabo. Aliquam fugiat velit quibusdam, aliquid nobis eveniet dolorem perspiciatis veritatis.</p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit a iste sint nam ipsa tempora neque laboriosam exercitationem? Sequi repudiandae, dicta necessitatibus laudantium aliquid fugiat est impedit recusandae iusto enim fugit harum odio quos aperiam qui accusamus amet, beatae animi id dolore quo pariatur. Facilis!</p>
+          <p>Welcome to Topizza, your ultimate destination for mouthwatering pizzas crafted with passion and perfection. At Topizza, we believe in bringing people together over the universal love for pizza. Every slice is a testament to our commitment to quality, taste, and innovation.</p>
+
+            <p>Our journey began with a simple goal: to create unforgettable pizza experiences. Using the finest, freshest ingredients and authentic recipes, we ensure every bite is a burst of flavor. Whether you crave the classics or adventurous gourmet toppings, we’ve got something to satisfy every palate.</p>
+
+            <p>But it’s not just about pizza—it’s about community. We pride ourselves on delivering not just food but happiness, be it through dine-in, delivery, or catering for your special moments.</p>
+
+            <p>At Topizza, we don’t just make pizzas—we make memories. Join us, and let’s create some delicious ones together!</p>
         </div>
       </section>
       <section className="text-center my-8" id="contact">
         <SectionHeaders subHeader={'Don\'t hesitate'} mainHeader={'Contact Us'} />
         <div className="mt-8">
-        <a className="text-4xl underline text-gray-500" href="tel:+918208605303">+91 82086 05303</a>
+          <a className="text-4xl underline text-gray-500" href="tel:+918208605303">+91 82086 05303</a>
         </div>
       </section>
-      
+
     </>
   );
 }
--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -1,19 +1,24 @@
 import Image from "next/image";
 import Right from "../icons/Right";
+import Link from "next/link";
 
-export default function Hero(){
-    return(
+export default function Hero() {
+    return (
         <section className="hero md:mt-4">
             <div className="py-8 md:py-12">
-            <h1 className="text-4xl font-semibold">Everything <br /> is better <br /> with a&nbsp;<span className="text-primary">Pizza</span></h1>
-            <p className="my-6 text-gray-500 text-sm">Pizza is the missing piece that makes every day complete, a simple yet delicious joy in life.</p>
-            <div className="flex gap-4 text-sm">
-                <button className="bg-primary flex justify-center items-center gap-2 uppercase text-white rounded-full px-4 py-2">Order Now <Right/></button>
-                <button className="flex border-0 items-center gap-2 py-2 text-gray-600 font-semibold">Learn More <Right/></button>
-            </div>
+                <h1 className="text-4xl font-semibold">Everything <br /> is better <br /> with a&nbsp;<span className="text-primary">Pizza</span></h1>
+                <p className="my-6 text-gray-500 text-sm">Pizza is the missing piece that makes every day complete, a simple yet delicious joy in life.</p>
+                <div className="flex gap-4 text-sm">
+                    <Link href={'/menu'}>
+                    <button className="bg-primary flex justify-center items-center gap-2 uppercase text-white rounded-full px-4 py-2">Order<Right /></button>
+                    </Link>
+                    <Link href={'/#about'}>
+                    <button className="flex border-0 items-center gap-2 py-2 text-gray-600 font-semibold">Learn More <Right /></button>
+                    </Link>
+                </div>
             </div>
             <div className="relative hidden md:block">
-            <Image src={'/pizza.png'} layout={'fill'} objectFit={'contain'} alt={"pizza"}/>
+                <Image src={'/pizza.png'} layout={'fill'} objectFit={'contain'} alt={"pizza"} />
             </div>
         </section>
     );
